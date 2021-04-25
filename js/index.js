39fae6386fffb0419616bf4f12412fd98ade2e11
prashantplusplus <81324922+prashantplusplus@users.noreply.github.com>
2021-04-24 17:55:28 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild( stats.dom );
 
 
+
 /**
  * read a file from the server.
  * @param {*} path - path to the file
